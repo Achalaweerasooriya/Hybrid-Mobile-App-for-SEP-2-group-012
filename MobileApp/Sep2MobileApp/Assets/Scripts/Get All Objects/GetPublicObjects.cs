66d4128ddf03d9fe8c1c4ef2e6eb7f5cd2c5c452
Achala <achala1994@gmail.com>
2016-09-17 23:55:33 +0530

    Adding ui mat and scene preparation

---
+++ MobileApp/Sep2MobileApp/Assets/Scripts/Get All Objects/GetPublicObjects.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+public class GetPublicObjects : MonoBehaviour {
+	public Text mNotificationText;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ MobileApp/Sep2MobileApp/Assets/Scripts/ScreenTransition/ScreenManager.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScreenManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void SetScreen(string screenname)
+	{
+		GameObject NewScreen = GameObject.Find (screenname);
+		NewScreen.transform.FindChild ("ScreenSpace").gameObject.SetActive(true);
+	}
+}
